ec6b8281147af0c6e15ae1ff87780c2a3fcb5899
Pete Forde <pete@unspace.ca>
2014-01-13 18:01:44 -0500

    stubs for error handling on external operations

--- chimp-minder.rb
+++ chimp-minder.rb
@@ -7,13 +7,22 @@ Dotenv.load
 gb = Gibbon::API.new
 gb.api_key = ENV["OVERRIDE_API_KEY"].empty? ? ENV["MAILCHIMP_API_KEY"] : ENV["OVERRIDE_API_KEY"]
 
-from_list = gb.lists.list({:filters => {:list_name => ENV["FROM_LIST"]}})
-from_list_id = from_list["data"][0]["id"]
+begin
+  from_list_id = gb.lists.list({:filters => {:list_name => ENV["FROM_LIST"]}})["data"][0]["id"]
+rescue Gibbon::MailChimpError => e
+  # e.message
+  # e.code
+end
 
-to_list = gb.lists.list({:filters => {:list_name => ENV["TO_LIST"]}})
-to_list_id = to_list["data"][0]["id"]
+begin
+  to_list_id = gb.lists.list({:filters => {:list_name => ENV["TO_LIST"]}})["data"][0]["id"]
+rescue Gibbon::MailChimpError => e
+end
 
-from_list_members = gb.lists.members({:id => from_list_id})["data"]
+begin
+  from_list_members = gb.lists.members({:id => from_list_id})["data"]
+rescue Gibbon::MailChimpError => e
+end
 
 from_list_members.each do |member|
   
@@ -26,22 +35,28 @@ from_list_members.each do |member|
     merge_vars[ENV["GUID_VAR"]] = SecureRandom.urlsafe_base64(nil, false)
   end
   
-  Gibbon::API.lists.subscribe({
-    :id => to_list_id, 
-    :email => {email: member["email"]}, 
-    :email_type => "html", 
-    :merge_vars => merge_vars, 
-    :double_optin => false, 
-    :update_existing => false, 
-    :send_welcome => false
-  })
+  begin    
+    Gibbon::API.lists.subscribe({
+      :id => to_list_id, 
+      :email => {email: member["email"]}, 
+      :email_type => "html", 
+      :merge_vars => merge_vars, 
+      :double_optin => false, 
+      :update_existing => false, 
+      :send_welcome => false
+    })
+  rescue Gibbon::MailChimpError => e
+  end
   
-  Gibbon::API.lists.unsubscribe({
-    :id => from_list_id,
-    :email => {email: member["email"]},
-    :delete_member => true,
-    :send_goodbye => false,
-    :send_notify => false
-  })
+  begin
+    Gibbon::API.lists.unsubscribe({
+      :id => from_list_id,
+      :email => {email: member["email"]},
+      :delete_member => true,
+      :send_goodbye => false,
+      :send_notify => false
+    })
+  rescue Gibbon::MailChimpError => e
+  end
 
 end
